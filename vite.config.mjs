322814567c908c82dcb95a37b4d8a3beeba70f03
LMX <2649078304@qq.com>
2025-02-20 19:43:34 +0800

    build: v1.0.0-bate-0

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,8 +3,9 @@
 export default {
   build: {
     lib: {
+      name: "VideoCropper",
       entry: ["src/index.ts"],
-      formats: ["es", "cjs"],
+      formats: ["es", "iife"],
       fileName: (format, entryName) =>
         `video-cropper-${entryName}.${format}.js`,
       cssFileName: "video-cropper"
