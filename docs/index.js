06c8e648be544b0447c7b31c54e21e078af44bc4
2649078304@qq.com <2649078304@qq.com>
2025-03-01 21:41:36 +0800

    docs: 发布doc

--- docs/index.js
+++ docs/index.js
@@ -1,36 +1,61 @@
 import VideoCropper from "../dist/video-cropper-index.es.js";
-
-const videoElement = document.getElementById("video");
-const playElement = document.getElementById("play");
-const pauseElement = document.getElementById("pause");
-const positionElement = document.getElementById("position");
-const rangeElement = document.getElementById("range");
-const previewElement = document.getElementById("preview");
-
-const videoCropper = new VideoCropper(videoElement, {
-  cropboxConfig: {
-    aspectRatio: 1
-  }
+var videoElement = document.getElementById("video");
+var playElement = document.getElementById("play");
+var pauseElement = document.getElementById("pause");
+var positionElement = document.getElementById("position");
+var rangeElement = document.getElementById("range");
+var previewElement = document.getElementById("preview");
+var exitPreviewElement = document.getElementById("exit-preview");
+var scaleMax = document.getElementById("scale-max");
+var scaleMin = document.getElementById("scale-min");
+var videoCropper = new VideoCropper(videoElement, {
+    cropBoxConfig: {
+        aspectRatio: 1
+        // position: {
+        //   x: 0,
+        //   y: 0,
+        //   width: 100,
+        //   height: 100
+        // }
+    },
+    videoConfig: {
+        muted: true
+    },
+    constraintBoxConfig: {
+    // position: {
+    //   x: 100,
+    //   y: 100,
+    //   width: 200,
+    //   height: 300
+    // }
+    }
 });
-const video = videoCropper.getVideo();
-
-videoCropper.setCropBoxPositionFunc((position) => {
-  positionElement.innerText = JSON.stringify(position);
+var video = videoCropper.getVideo();
+videoCropper.setCropBoxPositionFunc(function (nativePosition, renderPosition) {
+    positionElement.innerText = "".concat(JSON.stringify(nativePosition), " ").concat(JSON.stringify(renderPosition));
 });
-
-playElement.addEventListener("click", () => {
-  video.play();
+playElement.addEventListener("click", function () {
+    video.play();
 });
-
-pauseElement.addEventListener("click", () => {
-  video.pause();
+pauseElement.addEventListener("click", function () {
+    video.pause();
 });
-
-rangeElement.addEventListener("input", () => {
-  const time = (parseFloat(rangeElement.value) / 100) * video.duration;
-  video.setCurrentTime(time);
+rangeElement.addEventListener("input", function () {
+    var time = (parseFloat(rangeElement.value) / 100) * video.duration;
+    video.setCurrentTime(time);
 });
-
-previewElement.addEventListener("click", () => {
-  video.preview();
+previewElement.addEventListener("click", function () {
+    video.preview();
+});
+exitPreviewElement.addEventListener("click", function () {
+    video.exitPreview();
+});
+videoCropper.getVideo().setUpdateCallback(function (e) {
+    console.log(e);
+});
+scaleMax.addEventListener("click", function () {
+    videoCropper.scale(0.1);
+});
+scaleMin.addEventListener("click", function () {
+    videoCropper.scale(-0.1);
 });
